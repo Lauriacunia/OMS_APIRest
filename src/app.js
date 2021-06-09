13f23f99e88c5fc402e8f340faabd5b1b48f66d9
Lauriacunia <lauriacunia@gmail.com>
2021-06-09 19:53:39 -0300

    establezco conexion con la db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan= require('morgan');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Server on port 3000`)
+
+// conecting to db
+
+mongoose.connect('mongodb+srv://oms:*******@cluster0.cyfup.mongodb.net/omsCovid?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(db => console.log('Db connected'))
+  .catch(err => console.log(err));
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+
+app.use('/', indexRoutes);
+
+//starting the server
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
 })
